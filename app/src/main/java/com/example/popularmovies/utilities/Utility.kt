5dee63ed4d2743860c60bcef3025558f721ce37d
Carlos Rodriguez <carlosrz_isc@hotmail.com>
2017-06-12 13:02:22 -0500

    Singleton pattern in Kotlin is defined as object

--- app/src/main/java/com/example/popularmovies/utilities/Utility.kt
+++ app/src/main/java/com/example/popularmovies/utilities/Utility.kt
@@ -9,13 +9,11 @@ import android.net.ConnectivityManager
  * Utility class
  * Created by carlos on 6/8/17.
  */
-class Utility {
-    companion object {
-        fun hasInternetConnection(context: Context): Boolean {
-            val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+object Utility {
+    fun hasInternetConnection(context: Context): Boolean {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-            val activeNetwork = cm.activeNetworkInfo
-            return activeNetwork != null && activeNetwork.isConnectedOrConnecting
-        }
+        val activeNetwork = cm.activeNetworkInfo
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting
     }
 }
