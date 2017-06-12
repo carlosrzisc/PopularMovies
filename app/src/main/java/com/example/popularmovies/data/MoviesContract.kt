7260336812069e9c83045e17eaf0ae970670f6aa
Carlos Rodriguez <carlosrz_isc@hotmail.com>
2017-06-12 15:16:44 -0500

    Changing MoviesContract inner class to object

--- app/src/main/java/com/example/popularmovies/data/MoviesContract.kt
+++ app/src/main/java/com/example/popularmovies/data/MoviesContract.kt
@@ -14,28 +14,25 @@ object MoviesContract {
     val BASE_CONTENT_URI: Uri = Uri.parse("content://" + CONTENT_AUTHORITY)
     val PATH_MOVIE: String = "movie"
 
-    /* Inner class that setups up movie table structure */
-    class MovieEntry: BaseColumns {
-        companion object {
-            val MOVIES_TABLE = "movie"
-            val MOVIE_ID = "movie_id"
-            val TITLE = "title"
-            val POSTER = "poster"
-            val BACKDROP = "backdrop"
-            val OVERVIEW = "overview"
-            val VOTE_AVERAGE = "vote_average"
-            val RELEASE_DATE = "release_date"
+    object MovieEntry: BaseColumns {
+        val MOVIES_TABLE = "movie"
+        val MOVIE_ID = "movie_id"
+        val TITLE = "title"
+        val POSTER = "poster"
+        val BACKDROP = "backdrop"
+        val OVERVIEW = "overview"
+        val VOTE_AVERAGE = "vote_average"
+        val RELEASE_DATE = "release_date"
 
-            val CONTENT_URI: Uri =
-                    BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE).build()
-            val CONTENT_TYPE =
-                    ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE
-            val CONTENT_ITEM_TYPE =
-                    ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE
+        val CONTENT_URI: Uri =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE).build()
+        val CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE
+        val CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE
 
-            fun buildMovieUri(id:Long):Uri {
-                return ContentUris.withAppendedId(CONTENT_URI, id)
-            }
+        fun buildMovieUri(id:Long):Uri {
+            return ContentUris.withAppendedId(CONTENT_URI, id)
         }
     }
 }
